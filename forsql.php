676817374c91702cd73111576fff16abfceaf53f
Sadia Shahid <126645732+sadiatish08@users.noreply.github.com>
2023-05-08 12:07:58 +0600

    Delete forsql.php

--- forsql.php
+++
@@ -1,8 +0,0 @@
-<?php
-        $conn = mysqli_connect('localhost', 'root', '', 'database1');
-        if(mysqli_connect_errno()){
-            echo "connection filed";
-        }else{
-            echo"connection successful";
-        }
-        ?>
